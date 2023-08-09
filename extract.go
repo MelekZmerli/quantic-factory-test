28b08e3ee67063a47c9d3cfc79d9f77f6f2c047f
Malek Zmerli <zmerlimelek@gmail.com>
2023-08-09 22:19:13 +0100

    add protoype for extract file

---
+++ extract.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+    "fmt"
+    "io/ioutil"
+    "log"
+    "net/http"
+    "os"
+)
+
+func main() {
+    response, err := http.Get("https://opendata.paris.fr/api/records/1.0/search/?dataset=secteurs-des-bureaux-de-vote-en-2021&q=&rows=900&facet=arrondissement")
+
+    if err != nil {
+        fmt.Print(err.Error())
+        os.Exit(1)
+    }
+
+    responseData, err := ioutil.ReadAll(response.Body)
+    if err != nil {
+        log.Fatal(err)
+    }
+    fmt.Println(string(responseData))
+
+}
